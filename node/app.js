facdeadcacbcbc74f7a4f8ddcacd3bcb77441c8a
Rod Nolan <rodnolan@gmail.com>
2017-06-12 17:38:38 -0400

    removing code to handle postback messages
    since we removed the quick_reply options and the handler does nothing substantial, it's time to remove it completely

--- node/app.js
+++ node/app.js
@@ -139,10 +139,6 @@ app.post('/webhook', function (req, res) {
           // messenger platform sent a delivery confirmation
           receivedDeliveryConfirmation(messagingEvent);
 
-        } else if (messagingEvent.postback) {
-          // user replied by tapping one of our postback buttons
-          receivedPostback(messagingEvent);
-
         } else {
           console.log("[app.post] Webhook is not prepared to handle this message.");
 
@@ -227,27 +223,6 @@ function receivedDeliveryConfirmation(event) {
   }
 
   console.log("[receivedDeliveryConfirmation] All messages before timestamp %d were delivered.", watermark);
-}
-
-/*
- * Postback Event
- *
- * This event is called when a postback is tapped on a Structured Message. 
- * https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback-received
- * 
- */
-function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
-
-  // The 'payload' param is a developer-defined field which is set in a postback 
-  // button for Structured Messages. 
-  var payload = event.postback.payload;
-
-  console.log("[receivedPostback] from user (%d) on page (%d) with payload ('%s') " + 
-    "at (%d)", senderID, recipientID, payload, timeOfPostback);
-
 }
 
 /*
