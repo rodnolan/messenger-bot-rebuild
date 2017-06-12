d948e0df0b657cb74d8e4fec80f56b403045f42c
Rod Nolan <rodnolan@gmail.com>
2017-06-12 18:08:47 -0400

    removing helper function
    this is also orphan code now

--- node/app.js
+++ node/app.js
@@ -144,36 +144,6 @@ app.post('/webhook', function (req, res) {
 });
 
 /*
- * Call the Send API. The message data goes in the body. If successful, we'll 
- * get the message id in a response 
- *
- */
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      if (messageId) {
-        console.log("[callSendAPI] Successfully sent message with id %s to recipient %s", 
-          messageId, recipientId);
-      } else {
-      console.log("[callSendAPI] Successfully called Send API for recipient %s", 
-        recipientId);
-      }
-    } else {
-      console.error("[callSendAPI] Send API call failed", response.statusCode, response.statusMessage, body.error);
-    }
-  });  
-}
-
-/*
  * Start server
  * Webhooks must be available via SSL with a certificate signed by a valid 
  * certificate authority.
