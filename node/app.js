2ce4158e028b1f0d66102ab3be2b3b5e8f8acfca
Rod Nolan <rodnolan@gmail.com>
2017-06-12 18:46:55 -0400

    webhook validates falsely
    doesn't check the validation token

--- node/app.js
+++ node/app.js
@@ -66,14 +66,7 @@ if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
  *
  */
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    console.log("[app.get] Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
+  res.sendStatus(200);
 });
 
 
