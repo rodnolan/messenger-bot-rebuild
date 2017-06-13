8c490f3e298350350ddbb8d12358d83ab70bbd7d
Rod Nolan <rodnolan@gmail.com>
2017-06-12 20:17:19 -0400

    last minute tweaks

--- node/app.js
+++ node/app.js
@@ -19,39 +19,33 @@ const
   request = require('request');
 
 var app = express();
-app.set('port', process.env.PORT || 5000);
+app.set('port', 5000);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 /*
- * Open config/default.json and set your config values before running this code. 
- * You can also set them using environment variables.
- *
+ * Open config/default.json and set your config values before running this server
+ * You can restart the node server without reconfiguring anything
+ * However, whenever you restart ngrok you must (in this order):
+ *   1. reset the serverURL param in config\default.json
+ *   2. restart this node server 
+ *   3. revalidate the webhook url in your App Dashboard
  */
 
-// App Secret can be retrieved from the App Dashboard
-const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
-  process.env.MESSENGER_APP_SECRET :
-  config.get('appSecret');
+// App Dashboard > Dashboard > click the Show button in the App Secret field
+const APP_SECRET = config.get('appSecret');
 
-// Arbitrary value used to validate a webhook
-const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-  (process.env.MESSENGER_VALIDATION_TOKEN) :
-  config.get('validationToken');
+// App Dashboard > Webhooks > Edit Subscription > copy whatever random value you decide to use in the Verify Token field
+const VALIDATION_TOKEN = config.get('validationToken');
 
-// Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-  config.get('pageAccessToken');
+// App Dashboard > Messenger > Settings > Token Generation > select your page > copy the token that appears
+const PAGE_ACCESS_TOKEN = config.get('pageAccessToken');
 
-// URL where the app is running. Used to point to scripts and 
-// assets located at this address. DO NOT INCLUDE THE PROTOCOL
-const SERVER_URL = (process.env.SERVER_URL) ?
-  (process.env.SERVER_URL) :
-  config.get('serverURL');
+// Get this from your ngrok console but leave out the 'https://'
+// DO NOT INCLUDE THE PROTOCOL, it should just be [subdomain].ngrok.io
+const SERVER_URL = config.get('serverURL');
 
-// The protocol must be HTTPS so don't allow it to be configurable
-// avoid accidental misconfiguration by hard coding it
+// Avoid accidental misconfiguration by hard coding the protocol
 const IMG_BASE_PATH = 'https://' + SERVER_URL + "/assets/screenshots/";
 
 // make sure that everything has been properly configured
@@ -61,13 +55,10 @@ if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
 }
 
 /*
- * Start server
- * Webhooks must be available via SSL with a certificate signed by a valid 
- * certificate authority.
+ * Start your server
  */
 app.listen(app.get('port'), function() {
   console.log('[app.listen] Node app is running on port', app.get('port'));
 });
 
 module.exports = app;
-
