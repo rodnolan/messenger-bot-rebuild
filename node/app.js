f9277bce6e463bbc44c23e98d0d1970bf2e2bac7
Rod Nolan <rodnolan@gmail.com>
2017-06-14 18:16:05 -0400

    delete commented out code that got missed

--- node/app.js
+++ node/app.js
@@ -669,40 +669,10 @@ function callSendAPI(messageData) {
       console.error("[callSendAPI] code: %s subcode: %s message %s",
         error.code, error.error_subcode, error.message
       );
+      //console.error(response.statusCode, response.statusMessage, body.error);
     }
   });  
 }
-
-
-/*
- * Call the Send API. If the call succeeds, the message id is returned in the response.
- *
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      if (messageId) {
-        console.log("[callSendAPI] message id %s sent to recipient %s", 
-          messageId, recipientId);
-      } else {
-        console.log("[callSendAPI] called Send API for recipient %s", 
-          recipientId);
-      }
-    } else {
-      console.error("[callSendAPI] Send API call failed", response.statusCode, response.statusMessage, body.error);
-    }
-  });  
-}
- */
-
 
 /*
  * Start your server
